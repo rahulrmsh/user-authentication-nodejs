8e230ceecb7827f30a06f63c50c27712e0e84e0e
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-14 21:23:23 +0530

    added pu views

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+router.get('/logout', function(req, res, next) {
+    res.render('logout');
 });
 
 module.exports = router;
