5ab1ef7b91802edf8d3b4a6bb6dcea5f3da2e60e
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-15 22:21:16 +0530

    created mongodb models

---
+++ models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodeauth');
+var db = mongoose.connection;
+
+var UserSchema = mongoose.Schema({
+    inputUsername: {
+        type: String,
+        index: true
+    },
+    inputName: {
+        type: String
+    },
+    inputEmail: {
+        type: String
+    },
+    inputPassword: {
+        type: String
+    },
+    inputCheckPassword: {
+        type: String
+    },
+    profileimage: {
+        type: String
+    }
+});
+var User = mongoose.model.exports = mongoose.model('User', UserSchema);
+
+module.exports, createUser = function(newUser, callback) {
+    newUser.save(callback);
+}
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var expressValidator = require('express-validator');
+api.use(expressValidator())
+var api = express.Router();
 var router = express.Router();
 var multer = require('multer');
 var upload = multer({ dest: './uploads' });
@@ -12,7 +15,32 @@ router.get('/register', function(req, res, next) {
     res.render('register', { 'title': 'Register' });
 });
 router.post('/register', upload.single('profileimage'), function(req, res, next) {
-    console.log(req.body.inputName);
+    var inputName = req.body.inputName;
+    var inputEmail = req.body.inputEmail;
+    var inputUsername = req.body.inputUsername;
+    var inputPassword = req.body.inputPassword;
+    var inputCheckPassword = req.body.inputCheckPassword;
+    if (req.file) {
+        var profileimage = req.file.filename;
+    } else {
+        var profileimage = 'noimage.jpg';
+    }
+    req.checkBody('inputName', "Name field is required").notEmpty();
+    req.checkBody('inputEmail', "Email field is required").notEmpty();
+    req.checkBody('inputEmail', "Email is required").isEmail();
+    req.checkBody('inputUsername', "username field is required").notEmpty();
+    req.checkBody('inputPassword', "Password field is required").notEmpty();
+    req.checkBody('inputCheckPassword', "Password Incorrect").equals(req.body.inputPassword);
+    req.checkBody('name', "Name field is required").notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('register', {
+            errors: errors
+        });
+        console.log(errors);
+    } else {
+        console.log('No Errors');
+    }
 });
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
