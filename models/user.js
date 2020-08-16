d4967cab575221407d3ba8c387218d7c05a21742
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-16 22:52:09 +0530

    successfull node user create

--- models/user.js
+++ models/user.js
@@ -1,11 +1,20 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/nodeauth');
+const connectToMongoDB = () => {
+    const opts = {
+        keepAlive: true,
+        keepAliveInitialDelay: 300000,
+        socketTimeoutMS: 30000,
+        poolSize: 50,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 500,
+        autoReconnect: true,
+    };
 
-var db = mongoose.connection;
+    return mongoose.connect('mongodb://user:password@mongodb:27017/nodeauthz', opts);
+};
 
-// User Schema
 var UserSchema = mongoose.Schema({
     username: {
         type: String,
@@ -27,78 +36,7 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports.getUserById = function(id, callback) {
-    User.findById(id, callback);
-}
-
-module.exports.getUserByUsername = function(username, callback) {
-    var query = { username: username };
-    User.findOne(query, callback);
-}
-
-module.exports.comparePassword = function(candidatePassword, hash, callback) {
-    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-        callback(null, isMatch);
-    });
-}
-
 module.exports.createUser = function(newUser, callback) {
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(newUser.password, salt, function(err, hash) {
-            newUser.password = hash;
-            newUser.save(callback);
-        });
-    });
-}
-var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
-
-mongoose.connect('mongodb://localhost/nodeauth');
-
-var db = mongoose.connection;
-
-// User Schema
-var UserSchema = mongoose.Schema({
-    username: {
-        type: String,
-        index: true
-    },
-    password: {
-        type: String
-    },
-    email: {
-        type: String
-    },
-    name: {
-        type: String
-    },
-    profileimage: {
-        type: String
-    }
-});
-
-var User = module.exports = mongoose.model('User', UserSchema);
-
-module.exports.getUserById = function(id, callback) {
-    User.findById(id, callback);
-}
-
-module.exports.getUserByUsername = function(username, callback) {
-    var query = { username: username };
-    User.findOne(query, callback);
-}
-
-module.exports.comparePassword = function(candidatePassword, hash, callback) {
-    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-        callback(null, isMatch);
-    });
-}
-
-module.exports.createUser = function(newUser, callback) {
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(newUser.password, salt, function(err, hash) {
-            newUser.password = hash;
-            newUser.save(callback);
-        });
-    });
+    newUser.save(callback);
+    console.log('Success')
 }
